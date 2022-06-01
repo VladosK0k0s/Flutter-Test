554325bde1987c7406411fa2a6877424567ebcce
Vlad <stepaniuk@itirra.com>
2022-06-01 12:07:54 +0300

    added listView with random words

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -7,11 +8,45 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          colorScheme: ColorScheme.light().copyWith(primary: Colors.green[400]),
+          colorScheme: const ColorScheme.light().copyWith(primary: Colors.green[400]),
         ),
-        home: Scaffold(
-          appBar: AppBar(title: Text('App')),
-          body: Center(child: Text('Hello World')),
-        ));
+        home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final randomWordPairs = <WordPair>[];
+
+  Widget buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return const Divider();
+        final index = item ~/ 2;
+        if (item >= randomWordPairs.length) {
+          randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return buildRow(randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget buildRow(WordPair wordPair) {
+    return ListTile(
+        title: Text(wordPair.asPascalCase, style: const TextStyle(fontSize: 14)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test App')),
+      body: buildList(),
+    );
   }
 }
