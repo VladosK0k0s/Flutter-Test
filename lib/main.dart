aa611cf437b0978f2bab840cdf97e80bb9e3eced
Vlad <stepaniuk@itirra.com>
2022-06-01 13:53:56 +0300

    added animation playback test webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
 
-void main() => runApp(App());
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          colorScheme: const ColorScheme.light().copyWith(primary: Colors.green[400]),
+          colorScheme:
+              const ColorScheme.light().copyWith(primary: Colors.green[400]),
         ),
-        home: RandomWords());
+        home: const WebViewExample());
   }
 }
 
 class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
   @override
   RandomWordsState createState() => RandomWordsState();
 }
@@ -38,15 +42,33 @@ class RandomWordsState extends State<RandomWords> {
 
   Widget buildRow(WordPair wordPair) {
     return ListTile(
-        title: Text(wordPair.asPascalCase, style: const TextStyle(fontSize: 14)));
+        title:
+            Text(wordPair.asPascalCase, style: const TextStyle(fontSize: 14)));
   }
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return Scaffold(
       appBar: AppBar(title: const Text('Test App')),
       body: buildList(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  const WebViewExample({Key? key}) : super(key: key);
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  @override
+  Widget build(BuildContext context) {
+    return WebViewPlus(
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (controller) {
+        controller.loadUrl('assets/animation-playback/index.html');
+      },
     );
   }
 }
